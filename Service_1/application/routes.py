fa860199be6a39dec47744b284d621b9102f1e1b
Mark Rafferty <markrafferty27@gmail.com>
2020-07-24 07:04:01 +0000

    testing modifications

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -6,4 +6,4 @@ import requests
 @app.route('/', methods = ['GET'])
 def home():
     response = requests.get('http://service_4:5003').text
-    return render_template('home.html', title='Home', display = response)
+    return render_template('home.html', title='Home', pairing = response)
