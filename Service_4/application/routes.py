29827c0bbb004192a0db3148b7fe653f6bcfb513
Mark Rafferty <markrafferty27@gmail.com>
2020-07-24 11:01:34 +0000

    minor changes to test application

--- Service_4/application/routes.py
+++ Service_4/application/routes.py
@@ -4,8 +4,9 @@ import requests
 
 @app.route('/', methods = ['GET'])
 def generate_driver():
-    #driver = requests.get('http://Service_2:5001/driver').text
+    driver = requests.get('http://Service_2:5001/driver').text
     #team = requests.get('http://Service_3:5002/team').text
-    #pairing = "Driver " + driver + " will drive for " + team
-    pairing = "Driver Lewis Hamilton will drive for Red Bull"
+    team = "Red Bull"
+    pairing = "Driver " + driver + " will drive for " + team
+    #pairing = "Driver Lewis Hamilton will drive for Red Bull"
     return Response(pairing, mimetype='text/plain')
