8436c46f7ab8eab14c5c718bcba54ce014d1016a
Mark Rafferty <markrafferty27@gmail.com>
2020-07-24 07:19:32 +0000

    testing changes

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -5,5 +5,6 @@ import requests
 
 @app.route('/', methods = ['GET'])
 def home():
-    response = requests.get('http://service_4:5003').text
+    #response = requests.get('http://service_4:5003').text
+    responce = "Driver Lewis Hamilton will drive for Renault"
     return render_template('home.html', title='Home', pairing = response)
--- Service_4/application/routes.py
+++ Service_4/application/routes.py
@@ -5,6 +5,6 @@ import requests
 @app.route('/', methods = ['GET'])
 def generate_driver():
     driver = requests.get('http://Service_2:5001/driver').text
-    team = requests.get('http://Service_2:5002/team').text
-    display = "Driver " + driver + " will drive for " + team
-    return Response(display, mimetype='text/plain')
+    team = requests.get('http://Service_3:5002/team').text
+    pairing = "Driver " + driver + " will drive for " + team
+    return Response(pairing, mimetype='text/plain')
