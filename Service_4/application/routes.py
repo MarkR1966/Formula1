613a50330eb06dd2e747a4280f07c62e5248e4ec
Mark Rafferty <markrafferty27@gmail.com>
2020-07-28 09:02:03 +0000

    jenkinsfile

--- Service_4/application/routes.py
+++ Service_4/application/routes.py
@@ -5,8 +5,6 @@ import requests
 @app.route('/', methods = ['GET'])
 def generate_driver():
     driver = requests.get('http://Service_2:5001/driver').text
-    #team = requests.get('http://Service_3:5002/team').text
-    team = "Red Bull"
+    team = requests.get('http://Service_3:5002/team').text
     pairing = "Driver " + driver + " will drive for " + team
-    #pairing = "Driver Lewis Hamilton will drive for Red Bull"
     return Response(pairing, mimetype='text/plain')
