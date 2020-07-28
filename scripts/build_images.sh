938dad3e6838e3695bfdc1844b1f9c478423d432
Mark Rafferty <markrafferty27@gmail.com>
2020-07-28 10:44:43 +0000

    fixed build script looking at wrong image

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -8,10 +8,10 @@ if [[ "$(docker images -q markr1966/f1_service_2:latest 2> /dev/null)" == "" ]];
     docker build -t markr1966/f1_service_2 ./Service_2
 fi
 
-if [[ "$(docker images -q markr1966/f1_service_1:latest 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q markr1966/f1_service_3:latest 2> /dev/null)" == "" ]]; then
     docker build -t markr1966/f1_service_3 ./Service_3
 fi
 
-if [[ "$(docker images -q markr1966/f1_service_1:latest 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q markr1966/f1_service_4:latest 2> /dev/null)" == "" ]]; then
     docker build -t markr1966/f1_service_4 ./Service_4
 fi
