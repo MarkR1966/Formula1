84d869251348d85531670310247958b05909832b
Mark Rafferty <markrafferty27@gmail.com>
2020-07-29 19:38:07 +0000

    added functionality to display records on page

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -6,5 +6,6 @@ import requests
 
 @app.route('/', methods = ['GET'])
 def home():
-    response = requests.get('http://service_4:5003').text
-    return render_template('home.html', title='Home', pairing = response)
+    response = requests.get('http://service_4:5003').text                                           #Get response from Service_4
+    f1dat = F1DriversandTeams.select().order_by(-id.desc()).limit(5)                                #select last 5 records from the database to display on screen
+    return render_template('home.html', title='Home', pairing = response, f1dat = f1dat)            #render webpage showing response
