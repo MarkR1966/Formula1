d139c304a8a819e465b72598852e8852e862cf2b
MarkR1966 <markraferty27@gmail.com>
2020-07-30 17:28:22 +0000

    changes for database

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -6,6 +6,7 @@ import requests
 
 @app.route('/', methods = ['GET'])
 def home():
-    response = requests.get('http://service_4:5003').text                                           #Get response from Service_4
-    f1dat = F1DriversandTeams.query.order_by(id.desc()).limit(5)                                #select last 5 records from the database to display on screen
+    #response = requests.get('http://service_4:5003').text                                           #Get response from Service_4
+    response = "Lewis hamilton and Red Bull"
+    f1dat = F1DriversandTeams.query.order_by(id.desc())                                             #get records from database to display on web page
     return render_template('home.html', title='Home', pairing = response, f1dat = f1dat)            #render webpage showing response
