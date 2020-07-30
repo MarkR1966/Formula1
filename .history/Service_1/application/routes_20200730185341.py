697b771a7bf43ae229ed37ffd5a0f8cb3ffe49c1
MarkR1966 <markraferty27@gmail.com>
2020-07-30 21:53:02 +0000

    testing

---
+++ .history/Service_1/application/routes_20200730185341.py
@@ -0,0 +1,12 @@
+from application import app
+from flask import render_template
+from application.models import F1DriversandTeams
+
+import requests
+
+@app.route('/', methods = ['GET'])
+def home():
+    #response = requests.get('http://service_4:5003').text                                           #Get response from Service_4
+    response = "Lewis hamilton and Red Bull"
+    f1dat = F1DriversandTeams.query.order_by(Fid)                                             #get records from database to display on web page
+    return render_template('home.html', title='Home', pairing = response, f1dat = f1dat)            #render webpage showing response
