3720df17a13a247f135e2adb4a53fd40fe2a70e9
MarkR1966 <markraferty27@gmail.com>
2020-07-31 09:44:19 +0000

    database fixes

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -7,6 +7,5 @@ import requests
 @app.route('/', methods = ['GET'])
 def home():
     response = requests.get('http://service_4:5003').text                                           #Get response from Service_4
-    #response = "Lewis hamilton and Red Bull"
-    #f1dat = F1DriversandTeams.query.order_by(F1DriversandTeams.id)                                             #get records from database to display on web page
+    f1dat = F1DriversandTeams.query.order_by(F1DriversandTeams.id)                                             #get records from database to display on web page
     return render_template('home.html', title='Home', pairing = response)#, f1dat = f1dat)            #render webpage showing response
