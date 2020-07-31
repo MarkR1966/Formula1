f745a39d2d027d86c5d8376f4f57fbf8403a06ab
MarkR1966 <markraferty27@gmail.com>
2020-07-31 11:13:10 +0000

    jkgv

--- .history/Service_4/application/routes_20200731111245.py
+++ .history/Service_4/application/routes_20200731111245.py
@@ -6,7 +6,7 @@ import requests
 @app.route('/', methods = ['GET'])
 def generate_driver():
     r_driver = requests.get('http://service_2:5001/driver').text      #get result from Service_2
-    r_team = requests.get('http://service_3:5002/team').text          #get result from Service_3
+    r_team = requests.get('http://localhost:5002/team').text          #get result from Service_3
     pairing = "Driver " + r_driver + " will drive for " + r_team        #create response for Service_1
     #f1dat = F1dat(                                      #create record to persist data in db
     #        driver=r_driver,
