af260ee9c0b7f60ffb94b0d64e0f6d8b628f0ea1
MarkR1966 <markraferty27@gmail.com>
2020-07-31 11:08:24 +0000

    remove db changes

---
+++ .history/Service_4/application/models_20200731095950.py
@@ -0,0 +1,15 @@
+from application import db
+
+class F1dat(db.Model):                        # Define Class F1DriversandTeams to hold F1DriverandTeams table data
+    F1id = db.Column(db.Integer, primary_key=True)
+    driver = db.Column(db.String(30), nullable=False)
+    team = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return ''.join(
+            [
+                'F!datID: ' + str(self.id) + '\n'
+                'Driver: ' + self.driver + '\n'
+                'Team: ' + self.team + '\n'
+            ]
+        )
--- Service_4/application/models.py
+++ Service_4/application/models.py
@@ -1,14 +1,14 @@
 from application import db
 
-class F1DriversandTeams(db.Model):                        # Define Class F1DriversandTeams to hold F1DriverandTeams table data
-    id = db.Column(db.Integer, primary_key=True)
+class F1dat(db.Model):                        # Define Class F1DriversandTeams to hold F1DriverandTeams table data
+    f1id = db.Column(db.Integer, primary_key=True)
     driver = db.Column(db.String(30), nullable=False)
     team = db.Column(db.String(30), nullable=False)
 
     def __repr__(self):
         return ''.join(
             [
-                'F!datID: ' + str(self.id) + '\n'
+                'F1ID: ' + str(self.f1id) + '\n'
                 'Driver: ' + self.driver + '\n'
                 'Team: ' + self.team + '\n'
             ]
