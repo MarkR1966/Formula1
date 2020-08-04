809963ce7ee95efd10941b890ac5eb1b0251a080
MarkR1966 <markraferty27@gmail.com>
2020-08-04 10:01:26 +0000

    further environment variable fixes

---
+++ .history/scripts/build_services_20200730121551.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ "$(docker stack services f1dat 2> /dev/null)" == "" ]]; then
+    docker stack deploy --compose-file docker-compose.yml f1dat
+fi
+docker service update --image markr1966/f1_service_1:latest f1dat_service_1
+docker service update --image markr1966/f1_service_2:latest f1dat_service_2
+docker service update --image markr1966/f1_service_3:latest f1dat_service_3
+docker service update --image markr1966/f1_service_4:latest f1dat_service_4
+
+docker system prune -f
