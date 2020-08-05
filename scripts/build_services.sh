fe6e32a7c4e18180966a4bdebed4843eb35c863b
MarkR1966 <markrafferty27@gmail.com>
2020-08-05 17:07:54 +0100

    tidy up config and fix issues with nginx -5

--- scripts/build_services.sh
+++ scripts/build_services.sh
@@ -7,4 +7,4 @@ docker service update --image markr1966/f1_service_1:latest f1dat_service_1
 docker service update --image markr1966/f1_service_2:latest f1dat_service_2
 docker service update --image markr1966/f1_service_3:latest f1dat_service_3
 docker service update --image markr1966/f1_service_4:latest f1dat_service_4
-docker service update --image markr1966/nginx:latest nginx
+docker service update --image markr1966/nginx:latest f1dat_nginx
