1d1e14a5028c6fcae391442213699d4d5882e7db
MarkR1966 <markrafferty27@gmail.com>
2020-08-06 17:24:22 +0100

    Updated files with comments

--- Service_3/application/routes.py
+++ Service_3/application/routes.py
@@ -3,7 +3,7 @@ from flask import Flask, Response
 import random
 
 @app.route('/team', methods = ['GET'])
-def generate_team():
-    teams = ("Mercedes","Red Bull","Ferrari","Renault","McLaren")
-    choose_team =random.choice(teams)
-    return Response(choose_team, mimetype='text/plain')
+def generate_team():                                                                        # Generate random team for Service_4
+    teams = ("Mercedes","Red Bull","Ferrari","Renault","McLaren","Racing Point","AlphaTauri","Alfa Romeo Racing","Haas F1 Team","Williams")     # Generate list of teams
+    choose_team =random.choice(teams)                                                       # Select random team from the list
+    return Response(choose_team, mimetype='text/plain')                                     # Return team to Service_4
